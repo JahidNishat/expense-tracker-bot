3740662bb56f48d1a4b26ff7511b4ad439e7fd07
Masudur Rahman <masudjuly02@gmail.com>
2026-04-14 01:29:34 +0600

    Wire auth service into DI registry
    Signed-off-by: Masudur Rahman <masudjuly02@gmail.com>

--- services/all/all.go
+++ services/all/all.go
@@ -2,12 +2,15 @@ package all
 
 import (
 	"github.com/masudur-rahman/expense-tracker-bot/infra/logr"
+	authmod "github.com/masudur-rahman/expense-tracker-bot/modules/auth"
+	authrepo "github.com/masudur-rahman/expense-tracker-bot/repos/auth"
 	"github.com/masudur-rahman/expense-tracker-bot/repos/budgets"
 	"github.com/masudur-rahman/expense-tracker-bot/repos/event"
 	"github.com/masudur-rahman/expense-tracker-bot/repos/transaction"
 	"github.com/masudur-rahman/expense-tracker-bot/repos/user"
 	"github.com/masudur-rahman/expense-tracker-bot/repos/wallets"
 	"github.com/masudur-rahman/expense-tracker-bot/services"
+	authsvc "github.com/masudur-rahman/expense-tracker-bot/services/auth"
 	budgetsvc "github.com/masudur-rahman/expense-tracker-bot/services/budgets"
 	eventsvc "github.com/masudur-rahman/expense-tracker-bot/services/event"
 	txnsvc "github.com/masudur-rahman/expense-tracker-bot/services/transaction"
@@ -24,6 +27,7 @@ type Services struct {
 	Txn     services.TransactionService
 	Event   services.EventService
 	Budget  services.BudgetService
+	Auth    services.AuthService
 }
 
 var svc *Services
@@ -55,4 +59,20 @@ func InitiateSQLServices(uow styx.UnitOfWork, logger logr.Logger) {
 		Event:   eventSvc,
 		Budget:  budgetSvc,
 	}
+}
+
+// InitiateWebServices wires the auth service when the web dashboard is enabled.
+func InitiateWebServices(
+	messenger authmod.Messenger,
+	jwtSecret, refreshSecret, botUsername string,
+	uow styx.UnitOfWork,
+	logger logr.Logger,
+) {
+	userRepo := user.NewSQLUserRepository(uow.SQL, logger)
+	ar := authrepo.NewSQLAuthRepository(uow.SQL, logger)
+	svc.Auth = authsvc.NewAuthService(
+		userRepo, ar, messenger,
+		jwtSecret, refreshSecret, botUsername,
+		logger,
+	)
 }
